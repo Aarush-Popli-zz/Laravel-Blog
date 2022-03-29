f4eb561d1ea03b813da0150769578cd188151314
Aarush Popli <popliaarush00@gmail.com>
2022-03-29 20:00:21 +0530

    minor changes

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -7,7 +7,7 @@
     <!-- CSRF Token -->
     <meta name="csrf-token" content="{{ csrf_token() }}">
 
-    <title>{{ config('BlogIt', 'BlogIt') }}</title>
+    <title>Blog It</title>
 
     <!-- Scripts -->
     <script src="{{ asset('js/app.js') }}" defer></script>
@@ -26,7 +26,7 @@
         <nav class="navbar navbar-expand-md navbar-light bg-white shadow-sm">
             <div class="container">
                 <a class="navbar-brand fw-bold" href="{{ url('/') }}">
-                    {{ config('BlogIt', 'BlogIt') }}
+                    Blog It
                 </a>
                 <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="{{ __('Toggle navigation') }}">
                     <span class="navbar-toggler-icon"></span>
@@ -36,10 +36,10 @@
                     <!-- Left Side Of Navbar -->
                     <ul class="navbar-nav me-auto">
                     <a class="navbar-brand" href="{{ url('/home') }}">
-                            {{ config('Dashboard', 'Dashboard') }}
+                            Dashboard
                         </a>
                         <a class="navbar-brand" href="{{ url('/post') }}">
-                            {{ config('Post', 'Post') }}
+                            Post
                         </a>
                     </ul>
 
